b70c986b5ce1f9179d57ab308f1689c103d9365a
UeliKurmann <ueli.kurmann@gmail.com>
2024-07-12 20:45:37 +0200

    Restructure the code to simplify testing.

--- src/main/java/ch/javacamp/metrics/MetricsMojo.java
+++ src/main/java/ch/javacamp/metrics/MetricsMojo.java
@@ -1,7 +1,6 @@
 package ch.javacamp.metrics;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.project.MavenProject;
--- src/test/java/ch/javacamp/metrics/DependencyAnalyzerTest.java
+++ src/test/java/ch/javacamp/metrics/DependencyAnalyzerTest.java
@@ -1,7 +1,6 @@
 package ch.javacamp.metrics;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
